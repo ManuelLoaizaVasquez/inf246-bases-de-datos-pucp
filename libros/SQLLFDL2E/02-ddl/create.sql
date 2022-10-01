a764bf831cd7e54f9f77d76fedea7546ffcc8dc8
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2022-10-01 09:43:51 -0500

    Add 2.3 comments

--- libros/SQLLFDL2E/02-ddl/create.sql
+++ libros/SQLLFDL2E/02-ddl/create.sql
@@ -1,3 +1,5 @@
+-- 2.1 El comando CREATE
+
 CREATE TABLE cr_tarifa (
   id_tarifa NUMBER(38, 0),
   id_hotel NUMBER(38, 0),
@@ -19,3 +21,28 @@ CREATE TABLE cr_tipo_habitacion (
   tipo_cama VARCHAR2(50),
   descripcion VARCHAR2(255)
 );
+
+-- 2.2 Tablas temporales
+
+CREATE GLOBAL TEMPORARY TABLE cr_tarifas_2019 (
+  id_tarifa NUMBER(38, 0),
+  id_hotel NUMBER(38, 0),
+  id_tipo_habitacion NUMBER(38, 0),
+  fecha_inicio DATE,
+  fecha_fin DATE,
+  precio DECIMAL(7, 3)
+) ON COMMIT PRESERVE ROWS;
+
+-- 2.3 Los comentarios
+COMMENT ON TABLE cr_tarifa
+IS 'Tarifa por hotel y habitaciones';
+
+COMMENT ON COLUMN cr_hotel.id_hotel
+IS 'Numero del hotel';
+
+-- Los comentarios se almacenan en dos tablas Oracle
+SELECT *
+FROM user_tab_comments;
+
+SELECT *
+FROM user_col_comments;
--- libros/SQLLFDL2E/02-ddl/tablas-temporales.sql
+++
@@ -1,8 +0,0 @@
-CREATE GLOBAL TEMPORARY TABLE cr_tarifas_2019 (
-  id_tarifa NUMBER(38, 0),
-  id_hotel NUMBER(38, 0),
-  id_tipo_habitacion NUMBER(38, 0),
-  fecha_inicio DATE,
-  fecha_fin DATE,
-  precio DECIMAL(7, 3)
-) ON COMMIT PRESERVE ROWS;
