1d7d37e961f610133144727bc18a0c0af78619d6
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2022-10-07 09:10:32 -0500

    Add 8.3 valores por defecto

--- libros/SQLLFDL2E/02-ddl/integridad.sql
+++ libros/SQLLFDL2E/02-ddl/integridad.sql
@@ -34,3 +34,20 @@ CREATE TABLE cr_dormitorio (
   REFERENCES cr_tipo_habitacion (id_tipo_habitacion)
 );
 
+-- 8.3 Los valores por defecto
+CREATE TABLE cr_dormitorio (
+  id_habitacion INTEGER PRIMARY KEY,
+  id_hotel INTEGER,
+  id_tipo_habitacion INTEGER DEFAULT 2,
+  numero VARCHAR2(6),
+  comentario VARCHAR2(400),
+  CONSTRAINT fk_cr_tipo_habitacion FOREIGN KEY (id_tipo_habitacion)
+  REFERENCES cr_tipo_habitacion (id_tipo_habitacion)
+);
+
+ALTER TABLE cr_dormitorio
+MODIFY id_tipo_habitacion DEFAULT 3;
+
+ALTER TABLE cr_dormitorio
+ADD vista VARCHAR2(20) DEFAULT 'Mar';
+
